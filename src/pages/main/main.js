66074c3056350cdc7577eb358e2ad598f7db38f7
LEEINMOK <140570218+Lee-inmok@users.noreply.github.com>
2024-05-12 23:10:45 +0900

    save

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -21,7 +21,6 @@ export const Main = () => {
   const handleInputChange = (event) => {
     const value = event.target.value;
     setNumber(value);
-    localStorage.setItem("todoItem", value);
   };
 
   const handleSavePokemon = async () => {
--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container } from "./styled";
-import { useSelector } from "react-redux";
+import {useSelector } from "react-redux";
 import { Loadbook } from "../../components/loadbook/loadbook";
 
+
 export const Signin = () => {
+
   const savedPokemonbook = useSelector((state) => state.savedPokemonbook) || [];
   return (
     <Container>

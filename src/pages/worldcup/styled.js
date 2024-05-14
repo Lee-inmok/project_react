a29074e5fab16424df505a98c0cfcfce699857cd
Lee-inmok <adldlsahr1@naver.com>
2024-05-14 17:27:45 +0900

    worldcup SAVE(월드컵작동안함)
    ㅁㄴㅇ

---
+++ src/pages/worldcup/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const GameContainer = styled.div`
+display: flex;
+`;
+
+export const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  background-color: white;
+  cursor: pointer;
+`;
+
+export const ImgBox = styled.img`
+  width: 500px;
+  height: 500px;
+`;
+
+export const Game = styled.img`
+  font-size:35px;
+  font-weight:bold;
+`;
+
+export const Round = styled.img`
+  font-size:30px;
+  font-weight:bold;
+`;
